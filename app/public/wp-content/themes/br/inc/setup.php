484a751c18c4b6919d053382509a4083ccf6cb9f
chibariyo-yona <86947938+chibariyo-yona@users.noreply.github.com>
2026-04-23 13:01:02 +0900

    AdobeFont scipt変更

--- app/public/wp-content/themes/br/inc/setup.php
+++ app/public/wp-content/themes/br/inc/setup.php
@@ -725,14 +725,14 @@ function br_ceo_adobe_typekit() {
 	}
 	?>
 <script>
-(function(d) {
-	var config = {
-		kitId: 'res2dey',
-		scriptTimeout: 3000,
-		async: true
-	},
-	h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-})(document);
+  (function(d) {
+    var config = {
+      kitId: 'sjy1aot',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
 </script>
 	<?php
 }
